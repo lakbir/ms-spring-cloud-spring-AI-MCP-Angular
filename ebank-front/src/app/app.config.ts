4f67100d78a3307e4d7be0793a620d0fa5c49b14
lakbir <lakbir1993@gmail.com>
2026-05-14 00:59:38 +0200

    config interceptor

--- ebank-front/src/app/app.config.ts
+++ ebank-front/src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideMarkdown} from 'ngx-markdown';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {loadingInterceptor} from './interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideMarkdown(),
+    provideHttpClient(withInterceptors([loadingInterceptor])),
   ]
 };
